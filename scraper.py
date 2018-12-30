5a84e7fe014d7f4522be79d5dac4a6476e0c9b09
Carys Mills <carys.mills@shopify.com>
2018-12-29 23:10:02 -0500

    Switch back to phantomJS

--- scraper.py
+++ scraper.py
@@ -1,34 +1,34 @@
 from bs4 import BeautifulSoup  
 import sched, time
 import csv
-from selenium import webdriver
+from splinter import Browser
 
 def checkCanal():
-    url = "http://ncc-ccn.gc.ca/rideau-canal-skateway"
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.get(url)
-    html = driver.page_source
+	with Browser("phantomjs") as browser:
+	    browser.driver.set_window_size(1280, 1024)
+	    browser.visit("http://rcs.ncc-ccn.ca")
+	    button = browser.find_by_css(".feature-conditions")
+	    button.click()
+	    html = browser.html
 
-    soup = BeautifulSoup(html)
-    conditions = soup.find("div", id="canal-conditions")
-    readyText = soup.find("div", id="canal-header").find("h1").text
+        soup = BeautifulSoup(html)
+        conditions = soup.find("div", id="canal-conditions")
+        readyText = soup.find("div", id="canal-header").find("h1").text
 
-    if "partially open" in readyText:
-        ready = "partially open"
-    elif "open" in readyText:
-        ready = "open"
-    else:
-        ready = "closed"
+        if "partially open" in readyText:
+            ready = "partially open"
+        elif "open" in readyText:
+            ready = "open"
+        else:
+            ready = "closed"
 
 
-    info = conditions.find("p").text
-    infoformatted = info.encode('ascii', 'ignore')
-    datetime = (time.strftime("%d/%m/%Y %H:%M:%S"))
-    
-    with open('/var/www/canalstatus/canalstatus/canaldata.csv', 'a') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter="|")
-        csvwriter.writerow([ready, infoformatted, datetime])
+        info = conditions.find("p").text
+        infoformatted = info.encode('ascii', 'ignore')
+        datetime = (time.strftime("%d/%m/%Y %H:%M:%S"))
+        
+        with open('/var/www/canalstatus/canalstatus/canaldata.csv', 'a') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter="|")
+            csvwriter.writerow([ready, infoformatted, datetime])
         
 checkCanal();
