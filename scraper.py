75fb2f8494bb9be865b22d9584a492e9cb6f029f
Carys Mills <carys.mills@shopify.com>
2018-12-29 22:52:40 -0500

    Update scraper for Dec 2018 website changes

--- scraper.py
+++ scraper.py
@@ -1,25 +1,34 @@
 from bs4 import BeautifulSoup  
 import sched, time
 import csv
-from splinter import Browser
-import urllib2
+from selenium import webdriver
 
 def checkCanal():
-	quote_page = "http://ncc-ccn.gc.ca/places-to-visit/rideau-canal-skateway/"
-	page = urllib2.urlopen(quote_page)
+    url = "http://ncc-ccn.gc.ca/rideau-canal-skateway"
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get(url)
+    html = driver.page_source
 
-	ready = "false"
+    soup = BeautifulSoup(html)
+    conditions = soup.find("div", id="canal-conditions")
+    readyText = soup.find("div", id="canal-header").find("h1").text
 
-	soup = BeautifulSoup(page)
-	conditions = soup.find("div", attrs={"class": "card-content"})
-	status = conditions.find("p").text
-	infoformatted = status.encode('ascii', 'ignore')
-	print infoformatted
+    if "partially open" in readyText:
+        ready = "partially open"
+    elif "open" in readyText:
+        ready = "open"
+    else:
+        ready = "closed"
 
-	datetime = (time.strftime("%d/%m/%Y %H:%M:%S"))
 
-	with open('canaldata.csv', 'a') as csvfile:
-		csvwriter = csv.writer(csvfile, delimiter="|")
-		csvwriter.writerow([ready, infoformatted, datetime])
-
-checkCanal();	
+    info = conditions.find("p").text
+    infoformatted = info.encode('ascii', 'ignore')
+    datetime = (time.strftime("%d/%m/%Y %H:%M:%S"))
+    
+    with open('/var/www/canalstatus/canalstatus/canaldata.csv', 'a') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter="|")
+        csvwriter.writerow([ready, infoformatted, datetime])
+        
+checkCanal();
